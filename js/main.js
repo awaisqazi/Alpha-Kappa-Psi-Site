24b4db4d1bcfba5b44a972df374939497b2e5558
Antonio Mistretta <TonyTretta@users.noreply.github.com>
2017-05-31 21:45:43 -0700

    update brother portraits

---
+++ js/main.js
@@ -0,0 +1,83 @@
+var colonyValues = [
+                    {name: "Awais Qazi", position: "Founding President", imgURL: "assets/jpg/headshots/head-18.jpg"},
+                    {name: "Nikil Ghandi", position: "Founding Brother", imgURL: "assets/jpg/headshots/head-35.jpg"},
+                    {name: "Reza Wang-Lofti", position: "Founding Brother", imgURL: "assets/jpg/headshots/head-39.jpg"}
+                  ]
+
+var alphaValues = [
+                    {name: "Adit Patel", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Aidan McGourty", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Akash Sewani", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Annaleigh Potterton", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Antonio Mistretta", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Aris Rafael", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Arman Moussavi", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Astha Sharma", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Bindi Shah", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Christina De Ramos", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Christopher Yap", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Dhanush Sureshbabu", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Dhwanil Kadakia", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Dron Patel", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Dylan Kumenap", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Greg Buys", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "J'Ream Lewis", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Jacob Fattakhov", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Jacob Ponulak", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Jaideep Patel", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Jermayne Noseal", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Jessamine Mombay", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "John Shashaty", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Joshua Wolfe", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Kristen King", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Malay Patel", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Manogna Guduru", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Manoj George", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Mansha Kohli", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Mansi Sheth", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Michael Vaks", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Michael Armstrong Newson", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Nithin  Dronavalli", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Palash Jain", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Parth Vachani", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Riya Kurian", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Ross Jason Paayas-Pecson", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Rushabh Mehta", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Sanil Shah", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Shail Shah", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Sheryl Jacob", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Shivam Kanbur", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Shravanthi Budhi", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Steve Diaz", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Suzanne Hlinka", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Tapesh Nagarwal", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Vaibhav Pothireddy", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Vaibhava Pendyala", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Vasu Patel", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Vinaya Thadhani", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "William McCann", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Wissam Esmail", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"},
+                    {name: "Zohaeb Atiq", position: "Senior", imgURL: "assets/jpg/headshots/head-44.jpg"}
+                  ]
+
+function changeIMGs(array) {
+  newContent = "<div class='col-md-12 header'><h1>Meet Our Brothers</h1><hr /></div>";
+  for (var i = 0; i < array.length; i++) {
+    member = array[i];
+    var newMemberHTML = "<div class='col-md-3 headshot'><img src='"+member.imgURL+"' /><h2>"+member.name+"</h2><h2>"+member.position+"</h2></div>";
+    var newContent = newContent.concat(newMemberHTML);
+
+  }
+  document.getElementById("headshots").innerHTML = newContent;
+}
+
+$('#alpha').click(function(){
+    document.getElementById("brother-class").innerHTML = "Spring 2017";
+    changeIMGs(alphaValues);
+});
+$('#colony').click(function(){
+    document.getElementById("brother-class").innerHTML = "Fall 2016";
+    changeIMGs(colonyValues);
+});
+
+changeIMGs(alphaValues);
